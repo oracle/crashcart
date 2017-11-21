25ed6f5194e006d9017df8e1bcb045cb05f71694
Vishvananda Ishaya Abrams <vish.ishaya@oracle.com>
2017-11-21 13:34:26 -0800

    add the channel in the package build step

--- vol/build_crashcart.sh
+++ vol/build_crashcart.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 PACKAGES=${PACKAGES:-}
 
 cd /dev/crashcart/
+nix-channel --list | grep nixos-17.09 || nix-channel --add https://nixos.org/channels/nixos-17.09
 nix-channel --update
 rm -f profile
 nix-env -p profile -i ${PACKAGES}
